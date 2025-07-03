7de20f0b1f377d835ec69916be31cd41866425d3
jirehdeoca <jireh.deoca@makatihopeortigas.edu.ph>
2025-07-03 16:02:40 +0800

    Create OnboardingStack.js

---
+++ navigation/OnboardingStack.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingScreen from '../screens/OnboardingScreen';
+
+const Stack = createStackNavigator();
+
+export default function OnboardingStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+    </Stack.Navigator>
+  );
+}
