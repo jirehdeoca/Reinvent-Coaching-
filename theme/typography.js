6f22d1736218203d0271e30f00c687e02b17d773
jirehdeoca <jireh.deoca@makatihopeortigas.edu.ph>
2025-07-03 16:01:15 +0800

    Create typography.js

---
+++ theme/typography.js
@@ -0,0 +1,12 @@
+export default {
+  heading: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 24,
+    color: '#2D2D2D',
+  },
+  body: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 16,
+    color: '#2D2D2D',
+  },
+};
